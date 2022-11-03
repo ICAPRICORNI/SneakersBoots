db733c73915fbed36ee100c9e09f40a31f1588dc
notarat <criosonexus@gmail.com>
2022-11-03 19:35:15 +0200

    update columns

--- src/main/resources/db/migration/V2022.11.03.00.05__add_products_table.sql
+++ src/main/resources/db/migration/V2022.11.03.00.05__add_products_table.sql
@@ -1,9 +1,11 @@
 CREATE TABLE products
 (
     id          BIGSERIAL PRIMARY KEY,
-    name        TEXT    NOT NULL,
-    price       DECIMAL NOT NULL,
+    name        TEXT      NOT NULL,
+    price       DECIMAL   NOT NULL,
     old_price   DECIMAL,
     description TEXT,
-    status      TEXT    NOT NULL
+    brand_id    BIGINT    NOT NULL REFERENCES brands (id),
+    status      TEXT      NOT NULL,
+    created_at  TIMESTAMP NOT NULL DEFAULT now()
 )
